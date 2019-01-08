a1ba256db38f1df51828d1f462e28f5ec1bcc1d6
ghzlatarev <45011053+ghzlatarev@users.noreply.github.com>
2019-01-08 23:15:55 +0200

    Add files via upload

---
+++ Data Structures Speed Tests/Data Structures Speed Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Data_Structures_Speed_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Data Structures Speed Tests/HashSet VS List (Adding and Searching)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HashSet_VS_List__Adding_and_Searching_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var set = new HashSet<int>();
+            var list = new List<int>();
+            var total = 10000;
+
+            var sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < total; i++)
+            {
+                set.Add(i);
+            }
+            sw.Stop();
+            Console.WriteLine($"set add {total} items: {sw.ElapsedMilliseconds}ms");
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < total; i++)
+            {
+                list.Add(i);
+            }
+            sw.Stop();
+            Console.WriteLine($"list: add {total} items: {sw.ElapsedMilliseconds}ms");
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < total; i++)
+            {
+                set.Contains(i);
+            }
+            sw.Stop();
+            Console.WriteLine($"set: search {total} items: {sw.ElapsedMilliseconds}ms");
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < total; i++)
+            {
+                list.Contains(i);
+            }
+            sw.Stop();
+            Console.WriteLine($"list: search {total} items: {sw.ElapsedMilliseconds}ms");
+        }
+    }
+}
